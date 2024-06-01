60b401c9d1d42c0fdaf74f3d1f7ecac028eedf24
Andrés Choque Lopez <andreschoquelopez@gmail.com>
2024-05-31 21:52:43 -0300

    En el Main usar el paquete "db" para los concursos. Hacer uso de un archivo properties para configurar la conexión a la base de datos.

--- src/main/java/oo2/practico4/ejercicio3/main/Main.java
+++ src/main/java/oo2/practico4/ejercicio3/main/Main.java
@@ -1,5 +1,7 @@
 package oo2.practico4.ejercicio3.main;
 
+import oo2.practico4.ejercicio3.db.ConcursosDBService;
+import oo2.practico4.ejercicio3.db.DBParams;
 import oo2.practico4.ejercicio3.externo.ArchivoConcursos;
 import oo2.practico4.ejercicio3.externo.ArchivoInscripciones;
 import oo2.practico4.ejercicio3.modelo.Concursos;
@@ -9,7 +11,9 @@ import oo2.practico4.ejercicio3.modelo.Sistema;
 import oo2.practico4.ejercicio3.ui.VentanaRadioCompetition;
 
 import javax.swing.*;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Properties;
 
 public class Main {
 	public static void main(String[] args) {
@@ -26,10 +30,33 @@ public class Main {
 		});
 	}
 
-	private void start() throws URISyntaxException {
-		Concursos concursos = new ArchivoConcursos(Thread.currentThread().getContextClassLoader().getResource("concursos.txt").toURI());
-		Inscripciones inscripciones = new ArchivoInscripciones("inscriptos.txt");
+	private void start() throws URISyntaxException, IOException {
+		// Carga propiedades desde un archivo
+		Properties prop = new Properties();
+		prop.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties"));
+
+		//Concursos concursos = concursosPorArchivo();
+		Concursos concursos = concursosPorBaseDeDatos(prop);
+		Inscripciones inscripciones = inscripcionesPorArchivo();
 		Sistema sistema = new DefaultSistema(concursos, inscripciones);
 		new VentanaRadioCompetition(sistema);
+	}
+
+	private Concursos concursosPorArchivo() throws URISyntaxException {
+		return new ArchivoConcursos(Thread.currentThread().getContextClassLoader().getResource("concursos.txt").toURI());
+	}
+
+	private Concursos concursosPorBaseDeDatos(Properties prop) {
+		return new ConcursosDBService(parametrosDB(prop));
+	}
+
+	private Inscripciones inscripcionesPorArchivo() {
+		return new ArchivoInscripciones("inscriptos.txt");
+	}
+
+	private DBParams parametrosDB(Properties prop) {
+		String subprotocolo = prop.getProperty("jdbc.subprotocol"), subnombre = prop.getProperty("jdbc.subname.tp4e3");
+		String user = prop.getProperty("jdbc.user"), password = prop.getProperty("jdbc.password");
+		return new DBParams(subprotocolo, subnombre, user, password);
 	}
 }
